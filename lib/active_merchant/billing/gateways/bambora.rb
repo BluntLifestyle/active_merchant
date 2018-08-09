bb93771c374611232852375dfb08398fc14559c5
Maxime Gauthier <maxime.gauthier88@gmail.com>
2018-08-09 15:15:45 -0400

    Duplicate Beanstream files and rename to Bambora.

--- lib/active_merchant/billing/gateways/bambora.rb
+++ lib/active_merchant/billing/gateways/bambora.rb
@@ -1,37 +1,37 @@
-require 'active_merchant/billing/gateways/beanstream/beanstream_core'
+require 'active_merchant/billing/gateways/bambora/bambora_core'
 
 module ActiveMerchant #:nodoc:
   module Billing #:nodoc:
-    # This class implements the Canadian {Beanstream}[http://www.beanstream.com] payment gateway.
+    # This class implements the Canadian {Bambora}[http://www.bambora.com] payment gateway.
     # It is also named TD Canada Trust Online Mart payment gateway.
-    # To learn more about the specification of Beanstream gateway, please read the OM_Direct_Interface_API.pdf,
-    # which you can get from your Beanstream account or get from me by email.
+    # To learn more about the specification of Bambora gateway, please read the OM_Direct_Interface_API.pdf,
+    # which you can get from your Bambora account or get from me by email.
     #
-    # == Supported transaction types by Beanstream:
+    # == Supported transaction types by Bambora:
     # * +P+ - Purchase
     # * +PA+ - Pre Authorization
     # * +PAC+ - Pre Authorization Completion
     #
     # == Secure Payment Profiles:
-    # BeanStream supports payment profiles (vaults). This allows you to store cc information with BeanStream and process subsequent transactions with a customer id.
+    # Bambora supports payment profiles (vaults). This allows you to store cc information with Bambora and process subsequent transactions with a customer id.
     # Secure Payment Profiles must be enabled on your account (must be done over the phone).
     # Your API Access Passcode must be set in Administration => account settings => order settings.
-    # To learn more about storing credit cards with the Beanstream gateway, documentation can be found at http://developer.beanstream.com/documentation/classic-apis
+    # To learn more about storing credit cards with the Bambora gateway, documentation can be found at http://developer.bambora.com/documentation/classic-apis
     #
-    # To store a credit card using Beanstream's Legato Javascript Library (http://developer.beanstream.com/documentation/legato) you must pass the singleUseToken in
+    # To store a credit card using Bambora's Legato Javascript Library (http://developer.bambora.com/documentation/legato) you must pass the singleUseToken in
     # the store method's option parameter. Example: @gateway.store("gt6-0c78c25b-3637-4ba0-90e2-26105287f198")
     #
     # == Notes
     # * Adding of order products information is not implemented.
     # * Ensure that country and province data is provided as a code such as "CA", "US", "QC".
-    # * login is the Beanstream merchant ID, username and password should be enabled in your Beanstream account and passed in using the <tt>:user</tt> and <tt>:password</tt> options.
+    # * login is the Bambora merchant ID, username and password should be enabled in your Bambora account and passed in using the <tt>:user</tt> and <tt>:password</tt> options.
     # * Test your app with your true merchant id and test credit card information provided in the api pdf document.
-    # * Beanstream does not allow Payment Profiles to be deleted with their API. The accounts are 'closed', but have to be deleted manually.
+    # * Bambora does not allow Payment Profiles to be deleted with their API. The accounts are 'closed', but have to be deleted manually.
     #
-    #  Example authorization (Beanstream PA transaction type):
+    #  Example authorization (Bambora PA transaction type):
     #
     #   twenty = 2000
-    #   gateway = BeanstreamGateway.new(
+    #   gateway = BamboraGateway.new(
     #     :login => '100200000',
     #     :user => 'xiaobozz',
     #     :password => 'password'
@@ -64,8 +64,8 @@ module ActiveMerchant #:nodoc:
     #     :tax2 => 100,
     #     :custom => 'reference one'
     #   )
-    class BeanstreamGateway < Gateway
-      include BeanstreamCore
+    class BamboraGateway < Gateway
+      include BamboraCore
 
       def authorize(money, source, options = {})
         post = {}
@@ -148,7 +148,7 @@ module ActiveMerchant #:nodoc:
       end
 
       def interac
-        @interac ||= BeanstreamInteracGateway.new(@options)
+        @interac ||= BamboraInteracGateway.new(@options)
       end
 
       # To match the other stored-value gateways, like TrustCommerce,
